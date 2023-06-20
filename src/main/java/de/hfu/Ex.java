3b5a2e9f0c035e16e7cc15830bad76feb9c42926
sobottju <julian.benedikt.sobott@hs-furtwangen.de>
2023-06-20 17:53:01 +0200

    very basic roundtrip to set leds to red

--- src/main/java/de/hfu/Ex.java
+++ src/main/java/de/hfu/Ex.java
@@ -5,6 +5,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
@@ -13,14 +14,16 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @ApplicationScoped
 public class Ex {
 
     private static final String INPUT_TOPIC = "roskafka-hazards";
-    private static final String OUTPUT_TOPIC = "out_1";
+    private static final String OUTPUT_TOPIC = "kafkaros-leds";
 
     private static final Logger logger = LoggerFactory.getLogger(Ex.class);
 
@@ -34,13 +37,26 @@ public class Ex {
         StreamsBuilder builder = new StreamsBuilder();
 
         Serde<HazardDetectionVector> greetingSerde = new SpecificAvroSerde<>();
+        Serde<LightringLeds> lightringLedsSerde = new SpecificAvroSerde<>();
 
         final Map<String, String> serdeConfig = Collections.singletonMap("schema.registry.url", schemaRegistryUrl);
         greetingSerde.configure(serdeConfig, false);
+        lightringLedsSerde.configure(serdeConfig, false);
 
         builder.stream(INPUT_TOPIC, Consumed.with(Serdes.String(), greetingSerde))
-                .peek((key, value) -> logger.info("key: {}, value: {}", key, value));
-                //.to(OUTPUT_TOPIC, Produced.with(Serdes.String(), greetingSerde));
+                .filter((key, value) -> value.getDetections().size() > 0)
+                .peek((key, value) -> logger.info("key: {}, value: {}", key, value))
+                // all 6 leds red
+                .map((key, value) -> {
+                    List<LedColor> leds = new ArrayList<>();
+                    for (int i = 0; i < 6; i++) {
+                        leds.add(new LedColor(255, 0, 0));
+                    }
+                    int currentSeconds = (int) (System.currentTimeMillis() / 1000);
+                    Header header = new Header(new Time(currentSeconds, 0), "0");
+                    return new KeyValue<>(key, new LightringLeds(header, leds, false));
+                })
+                .to(OUTPUT_TOPIC, Produced.with(Serdes.String(), lightringLedsSerde));
 
         return builder.build();
     }
